dfb44a3e7994827d1adc673ad33f72f5fbed2ad0
Brandon Hang <brandonhang34@outlook.com>
2022-03-09 12:31:28 -0800

    Lab 2 completed

--- stamp_program.py
+++ stamp_program.py
@@ -8,14 +8,18 @@ end
 '''
 
 import math
+#input: sheet
 
 def calculate(sheet):
+    #step 1
     answer = sheet/3
+    #step 2
     rounded = math.ceil(answer)
     print("Sheet is: ", sheet)
     print("The answer is: ", answer)
     print("Rounded is: ", rounded)
     print("====================================")
+    #output: number of stamps needed
     return rounded
 
 output = calculate(16)
